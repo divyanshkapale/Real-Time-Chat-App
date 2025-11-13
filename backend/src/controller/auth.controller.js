4434587be6f14de18f24e2ca47f21d631f3e2070
Divyanshu Kapale <divyanshkapale@gmail.com>
2025-11-13 21:32:57 +0530

    signup

---
+++ backend/src/controller/auth.controller.js
@@ -0,0 +1,57 @@
+import User from "../model/User.model.js";
+import bcrypt from "bcrypt"
+import { generateToken } from "../lib/utils.js";
+
+export const signup = async (req, res) => {
+  const { fullName, email, password } = req.body;
+
+  try {
+    if (!fullName || !email || !password) {
+      return res.status(400).json({ message: "All Field are required" });
+    }
+    if (password.length < 6) {
+      return res
+        .status(400)
+        .json({ message: "password must be at least 6 Character" });
+    }
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+    if (!emailRegex.test(email)) {
+      return res.status(400).json({ message: "Invalid email format" });
+    }
+
+    const user=await User.findOne({email:email})
+
+    if(user){
+        return res.status(400).json({message:"Email Already exists"})
+    }
+
+    const salt = await bcrypt.genSalt(10);
+
+    const hashedPassword= await bcrypt.hash(password,salt)
+
+    const newUser =new User({
+        fullName,
+        email,
+        password:hashedPassword
+    })
+
+    if(newUser){
+        generateToken(newUser._id,res);
+        await newUser.save();
+        res.status(201).json({
+            _id:newUser._id,
+            fullName:newUser.fullName,
+            email:newUser.email,
+            profilePic:newUser.profilePic
+        })
+    }
+    else {
+        res.status(400).json({message:"invalid User Data "})
+    }
+
+  } catch (error) {
+    console.log("Error in Signup Controller",error)
+    res.status(500).json({message:"Internal Server Error"})
+  }
+};
--- backend/src/server.js
+++ backend/src/server.js
@@ -2,14 +2,17 @@ import express from "express";
 import dotenv from "dotenv";
 import authRoute from "./routes/auth.routes.js";
 import messageRoute from "./routes/message.route.js";
+import { connectDB } from "./lib/db.js";
 const app = express();
 dotenv.config();
 
 const port = process.env.PORT || 3000;
 
+app.use(express.json()) // req.body
 app.use("/api/auth", authRoute);
 app.use("/api/messages", messageRoute);
 
 app.listen(port, () => {
   console.log(`Server is running on ${port}`);
+  connectDB()
 });
